536f5b88fae4d10f4eb8e0b075f068e2d7e9c92d
saitejaswi74 <saitejaswibojja@gmail.com>
2018-10-31 17:55:15 +0530

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- 13.1 isMinHeap/Solution.java
+++ 13.1 isMinHeap/Solution.java
@@ -1,5 +1,4 @@
 
-
 import java.util.Scanner;
 
 class Heap
@@ -81,24 +80,60 @@ public class Solution
 				}
 				break;
 			case "Float":
-				
-				break;
-			case "Double":
-				break;
-			case "Integer":
 				int n1=sc.nextInt();
 				String[] z=new String[n1];
 				String z1=sc.nextLine();
+				
 				for(int i=0;i<z.length;i++)
 				{
 					String z2=sc.next();
 					String z3[]=null;
 					z3=z2.split(",");
-					Double[] d=new Double[z3.length];
-					for(int l=0;l<z3.length;i++)
+					Double[] d=new Double[z3.length+1];
+					for(int l=1;l<=z3.length;i++)
 					{
-						String z4=z3[l];
-						d[l]=Double.parseDouble(z4);
+						//String z4=z3[l];
+						d[l]=Double.parseDouble(z3[l-1]);
+					}
+					Heap h=new Heap();
+					System.out.println(h.Min_num(1, d));
+				}
+				break;
+			case "Double":
+				int n2=sc.nextInt();
+				String[] y=new String[n2];
+				String y1=sc.nextLine();
+				
+				for(int i=0;i<y.length;i++)
+				{
+					String y2=sc.next();
+					String y3[]=null;
+					y3=y2.split(",");
+					Double[] d=new Double[y3.length+1];
+					for(int l=1;l<=y3.length;i++)
+					{
+						//String z4=z3[l];
+						d[l]=Double.parseDouble(y3[l-1]);
+					}
+					Heap h=new Heap();
+					System.out.println(h.Min_num(1, d));
+				}
+				break;
+			case "Integer":
+				int n3=sc.nextInt();
+				String[] x=new String[n3];
+				String x1=sc.nextLine();
+				
+				for(int i=0;i<x.length;i++)
+				{
+					String x2=sc.next();
+					String x3[]=null;
+					x3=x2.split(",");
+					Double[] d=new Double[x3.length+1];
+					for(int l=1;l<=x3.length;i++)
+					{
+						//String z4=z3[l];
+						d[l]=Double.parseDouble(x3[l-1]);
 					}
 					Heap h=new Heap();
 					System.out.println(h.Min_num(1, d));
